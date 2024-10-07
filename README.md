0b1d4a2fe4b13a3f30ea7216fa09dc61b77c9d64
antoniofregoso <antoniofregoso@protonmail.com>
2024-10-06 19:43:47 -0600

    [ADD] first commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# create-bj-app
+# @buyerjourney/create-bj-app
 Create a minimal BJ App
+## Sponsors
+[<img src="https://www.conference.com.mx/web/image/website/3/logo/Conference?unique=cb769b7">](https://www.conference.com.mx/comercializacion-digital)
+
+## Installation
+```npm i @buyerjourney/create-bj-app```
+## Usage
+```npx @buyerjourney/create-bj-app app-name```
+
+A basic bj app will be created with a header, hero banner and footer with the following functionalities:
+1. State management.
+2. Language change.
+3. Dark and ligth theme change.
+## License
+create-bj-app is [GPL-3.0-or-later](./LICENSE).
---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const degit = require('degit');
+const path = require('path');
+const fs = require('fs');
+
+
+// Check if the project name was passed as an argument
+const projectName = process.argv[2];
+if (!projectName) {
+  console.error('Please provide a name for the project.');
+  process.exit(1);
+}
+
+// Project route
+const projectPath = path.join(process.cwd(), projectName);
+
+// Check if the project folder already exists
+if (fs.existsSync(projectPath)) {
+    console.error(`The directory ${projectName} already exists.`);
+    process.exit(1);
+  }
+  
+// Configure `degit` to clone GitHub repository
+const emitter = degit('antoniofregoso/bj-app', {
+    cache: false,
+    force: true,
+    verbose: true
+  });
+
+  // Start cloning
+emitter.clone(projectPath).then(() => {
+    console.log(`Project ${projectName} successfully created from GitHub!`);
+    console.log('1. Go to the project directory:');
+    console.log(`   cd ${projectName}`);
+    console.log('2. Install the dependencies:');
+    console.log('   npm install');
+    console.log('3. Start the application:');
+    console.log('   npm start');
+  }).catch(err => {
+    console.error('Error cloning repository:', err);
+  });
+  
